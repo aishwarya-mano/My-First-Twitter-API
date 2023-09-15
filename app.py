1d16d3afe97a797306606a7b8a33ed7b4da73920
Aishwarya Manoharan <143560747+aishwarya-mano@users.noreply.github.com>
2023-09-15 23:40:33 +0530

    Adding new app.py

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import tweepy
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
+bearer_token = config['twitter']['bearer_token']
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/create", methods=['POST'])
+def create_tweet():
+    req = request.json
+    tweet_text = req['text']
+
+    client = tweepy.Client(bearer_token, api_key,
+                           api_key_secret, access_token, access_token_secret)
+    try:
+        response = client.create_tweet(text=tweet_text)
+        print(response)
+        return jsonify(isError=False,
+                       message="Success",
+                       statusCode=200,
+                       data=response)
+    except:
+        return jsonify(isError=True,
+                       message="Failed",
+                       statusCode=500,
+                       data={})
+
+@app.route("/delete/<tweet_id>", methods=['DELETE'])
+def delete_tweet(tweet_id):
+    client = tweepy.Client(bearer_token, api_key,
+                           api_key_secret, access_token, access_token_secret)
+    try:
+        response = client.delete_tweet(id=tweet_id)
+        print(response)
+        return jsonify(isError=False,
+                       message="Success",
+                       statusCode=200,
+                       data=response)
+    except:
+        return jsonify(isError=True,
+                       message="Failed",
+                       statusCode=500,
+                       data={})

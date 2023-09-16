90e2e9f9e9e678ebc76b16c61a34143c5a35d23b
upasanakr <144417727+upasanakr@users.noreply.github.com>
2023-09-16 14:29:23 -0700

    Update test.py
    Added new test cases for failed scenarios

--- test.py
+++ test.py
@@ -2,8 +2,12 @@ import unittest
 from unittest.mock import patch
 from app import app
 
+class TwitterAPIValidationError(Exception):
+     def __init__(self, message, api_messages):            
+        super().__init__(message)
+        self.api_messages = api_messages
+
 class TestFlaskApp(unittest.TestCase):
-  
     def setUp(self):
         app.config['TESTING'] = True
         self.client = app.test_client()
@@ -20,12 +24,26 @@ class TestFlaskApp(unittest.TestCase):
         mock_tweepy_client = mock_client.return_value 
         mock_tweepy_client.create_tweet.return_value = mock_response
         response = self.client.post('/create', json={'text': 'Test tweet'})
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json()["statusCode"], 200)
         self.assertEqual(response.get_json(), {
             "isError": False,
             "message": "Success",
             "statusCode": 200,
             "data": {"edit_history_tweet_ids": ["1702815938582806759"], "id": "1702815938582806759", "text": "Test tweet"}
+        })
+    
+    @patch('app.tweepy.Client')
+    def test_create_tweet_failure(self, mock_client):
+        mock_tweepy_client = mock_client.return_value
+        mock_tweepy_client.create_tweet.side_effect = TwitterAPIValidationError("Failed", [""])
+        client = app.test_client()
+        response = client.post('/create', json={'text': 'Test tweet'})
+        self.assertEqual(response.get_json()["statusCode"], 500)
+        self.assertEqual(response.get_json(), {
+            "isError": True,
+            "message": "",
+            "statusCode": 500,
+            "data": {}
         })
     
     @patch('app.tweepy.Client')
@@ -37,13 +55,27 @@ class TestFlaskApp(unittest.TestCase):
         mock_tweepy_client.delete_tweet.return_value = mock_response
         client = app.test_client()
         response = client.delete('/delete/12345')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.get_json()["statusCode"], 200)
         self.assertEqual(response.get_json(), {
             "isError": False,
             "message": "Success",
             "statusCode": 200,
             "data": { "deleted": True}
         })
+
+    @patch('app.tweepy.Client')
+    def test_delete_tweet_failure(self, mock_client):
+        mock_tweepy_client = mock_client.return_value
+        mock_tweepy_client.delete_tweet.side_effect = Exception("Failed")
+        client = app.test_client()
+        response = client.delete('/delete/12345')
+        self.assertEqual(response.get_json()["statusCode"], 500)
+        self.assertEqual(response.get_json(), {
+            "isError": True,
+            "message": "Failed",
+            "statusCode": 500,
+            "data": {}
+        }) 
 
 if __name__ == '__main__':
     unittest.main()
